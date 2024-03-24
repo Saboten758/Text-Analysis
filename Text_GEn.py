970aad0312511ab360da55dfeeec5b6e5fa57c85
Saboten 758 <debjitdaw03@gmail.com>
2024-03-24 16:24:21 +0530

    yep

--- Text_GEn.py
+++ Text_GEn.py
@@ -154,7 +154,7 @@ def music_gen(txt,time_given):
 def music_page():
     st.write("# This Page Generates Music from Text🎵")
     st.write("Enter Your Prompt below to generate some music:")
-    prompt = st.text_area("Input Text", "")
+    prompt = st.text_area("Enter Below:", "")
     time_given= 5
     time_given= st.slider("How Many Seconds to generate (Note: Increasing this may lead to longer generation times):", 5, 30)
     if st.button("Generate"):
